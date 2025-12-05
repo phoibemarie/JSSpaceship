e391f18e078fa00677245e31ad2c87f7f9f7da0b
Faith Marie <144547716+phoibemarie@users.noreply.github.com>
2025-12-05 18:50:11 -0500

    Add files via upload
    decided to develop a spaceship animation using variables and functions

--- sketch.js
+++ sketch.js
@@ -2,50 +2,53 @@ function setup() {
   createCanvas(400, 400);
 }
 
-function draw() {
-  
-//Changing the background color RGB
-//must be at the top of the program or it will overlay the other code
-  background(164, 196, 0)
-  
-  
-//Variables 
-  
-  
-    // turn off stroke for face shapes
-  noStroke();
+
+//position of the spaceship
+var x = 200; 
+var y = 200;
+var shipHeight = 200
+var shipWidth = 200
+
+//create a variable to control the ship's lights 
+  //Now if I want to change it, i only have to change 1 variable instead of 6
+     //also shifts the objects simultaneously to retain relative orientation
+var shipLightsSize = 30; 
 
   
-  //The Ellipse Function 
-
-//where to draw the ellipse and what size 
-//ellipse(how far the ellipse from the left side, moves vertically, makes wider/thinner, changes height)
-//the first two parameters control the position, the second two the size
-      //ellipse(x, y, width, height )
-  
-// face
-  //fill function - will color every shape that comes after this command
-
-fill(130,90,44)
-ellipse(202, 208, 300, 300);
-  
-// eyes
-fill(0,0,0);
-ellipse(157, 151, 40, 40);
-ellipse(304, 142, 40, 40);
-
-// mouth
-fill(255, 0, 0)
-ellipse(257, 240, 120, 136);
-  
-//Making Rectangles - rect()
-  //(x,y, width, height)
-  //parameters adjust based on the upper left corner of the rectangle
-fill(130,90,44)
-rect(160, 350, 95, 50)
  
-strokeWeight(33);
-stroke(250,0,242);
-line(83,116, 271,74);
+draw = function() {
+  // all lines of code inside here will be run repeatedly
+  
+background(0,0,0);
+  
+  
+//spaceship cockpit 
+fill(166,226,75)
+ellipse(x,y-25, shipWidth-50, shipHeight);
+  
+//spaceshipbottom
+fill(255,255,0)
+ellipse(x,y+20, shipWidth-100, shipHeight-60);
+ellipse(x-40,y+15, shipWidth-100, shipHeight-60)
+ellipse(x+40,y+15, shipWidth-100, shipHeight-60)
+  
+//spaceship body
+fill(119,151,56)
+ellipse(x,y,shipWidth+50, shipHeight-60);
 
-}
+fill(255,0,0)
+//line(x1, y1, x2, y2) --> ccoordinates for each end point
+line(x-125, y, x+125, y)
+  
+//spaceship lights 
+fill(255,255,0)
+ellipse(x-50,y, shipLightsSize,shipLightsSize);
+ellipse(x,y,shipLightsSize,shipLightsSize);
+ellipse(x+50,y, shipLightsSize,shipLightsSize);
+  
+//to change position for movement
+//for speed increase or decrease x 
+//for direction, use (-) for R to L
+// x = x+5; 
+
+};
